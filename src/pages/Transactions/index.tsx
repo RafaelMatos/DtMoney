c61879c27c67c0cfbf11920ca8f2f7c28b40f698
RafaelMatos <rafael-matos@outlook.com>
2023-03-20 18:34:23 -0300

    feat(table):create transactions table

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,12 +1,39 @@
-import { Header } from "../../components/Header"
-import { Summary } from "../../components/Summary"
+import { Header } from "../../components/Header";
+import { Summary } from "../../components/Summary";
+import { TramsactionsContainer,TransctionsTable, PriceHighlight } from "./styles";
 
-export function Transactions(){
-  return(
+export function Transactions() {
+  return (
     <div>
-      <Header/>
-      <Summary/>
-    <div>Transações</div>
+      <Header />
+      <Summary />
+      <TramsactionsContainer>
+        <TransctionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+              <PriceHighlight variant="income">
+                  R$ 12.000,00
+                  </PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>20/03/2022</td>
+            </tr>
+            <tr>
+              <td width="50%">Hambúrguer</td>
+              <td>
+                <PriceHighlight variant="outcome">
+                  - R$ 59,00
+                </PriceHighlight>
+              </td>
+              <td>Alimentação</td>
+              <td>20/03/2022</td>
+            </tr>
+          </tbody>
+        </TransctionsTable>
+      </TramsactionsContainer>
+      <div>Transações</div>
     </div>
-  )
+  );
 }
